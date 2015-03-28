74867d87d6a65c957a42bcf553af6486b5ff88d5
Ross Harper <ross@rossharper.net>
2015-03-28 16:58:24 +0000

    first commit from pi

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello Node!\n');
+    response.end('Hello Pi Node!\n');
 }).listen(8080);
 
