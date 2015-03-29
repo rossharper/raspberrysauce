35aaec06600d7049c1479b92862a5360e740a971
rossharper <ross@rossharper.net>
2015-03-29 15:03:58 +0100

    simplified authentication

--- routes/index.js
+++ routes/index.js
@@ -17,22 +17,12 @@ function initRoutes() {
         })
     });
 
-    router.post('/login', function(req, res, next) {
-        passport.authenticate('local', function(err, user, info) {
-            if (err) {
-                return next(err)
-            }
-            if (!user) {
-                return res.redirect('/login')
-            }
-            req.logIn(user, function(err) {
-                if (err) {
-                    return next(err);
-                }
-                return res.redirect('/');
-            });
-        })(req, res, next);
-    });
+    router.post('/login', 
+        passport.authenticate('local', 
+                                { 
+                                    successRedirect: '/',
+                                    failureRedirect: '/login' 
+                                }));
 
     router.get('/logout', function(req, res) {
         req.logout();
