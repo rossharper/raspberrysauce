c364648c92190c1bcc4fa9cb5075a3bb3543b7b3
Ross Harper <rossharper@users.noreply.github.com>
2015-03-29 15:43:52 +0100

    breaking authentication setup from main

--- auth/Authentication.js
+++ auth/Authentication.js
@@ -1,6 +1,66 @@
-var passport = require('passport');
+var passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    pass = require('pwd');
+
+var users = require('../models/users');
+
+function initPassport() {
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        users.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+
+    passport.use('local', new LocalStrategy(
+        function(username, password, done) {
+            // asynchronous verification, for effect...
+            process.nextTick(function() {
+
+                // Find the user by username.  If there is no user with the given
+                // username, or the password is not correct, set the user to `false` to
+                // indicate failure and set a flash message.  Otherwise, return the
+                // authenticated `user`.
+                users.findByUsername(username, function(err, user) {
+                    if (err) {
+                        return done(err);
+                    }
+                    if (!user) {
+                        return done(null, false, {
+                            message: 'Unknown user ' + username
+                        });
+                    }
+                    pass.hash(password, user.salt, function(err, hash) {
+                        console.log("sub hash: " + hash);
+                        console.log("usr hash: " + user.password);
+                        if(err) { 
+                            return done(err); 
+                        }
+                        if (user.password == hash) {
+                            done(null, user);
+                        }
+                        else {
+                            return done(null, false, {
+                                message: 'Invalid password'
+                            });
+                        }
+                    });
+                })
+            });
+        }
+    ));
+}
 
 var auth = {
+    initialize: function(app) {
+        initPassport();
+        app.use(passport.initialize());
+        app.use(passport.session());
+    }
+    ,
     getAuthenticationHandler: function(authenticationRedirects) {
         return passport.authenticate(
             'local', authenticationRedirects);
--- index.js
+++ index.js
@@ -9,72 +9,19 @@ var express = require('express'),
     morgan = require('morgan'),
     stylus = require('stylus'),
     nib = require('nib'),
-    passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy,
     cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser'),
-    pass = require('pwd');
+    bodyParser = require('body-parser');
 
 /*
  * Submodule dependencies
  */
-var users = require('./models/users'),
+var auth = require('./auth/Authentication');
     routes = require('./routes/index'),
     secureServer = require('./servers/SecureServer');
     inSecureRedirect = require('./servers/RedirectingInsecureServer');
 
 var securePort = process.argv[2] || 4443,
     insecurePort = process.argv[3] || 8080;
-
-function initPassport() {
-    passport.serializeUser(function(user, done) {
-        done(null, user.id);
-    });
-
-    passport.deserializeUser(function(id, done) {
-        users.findById(id, function(err, user) {
-            done(err, user);
-        });
-    });
-
-    passport.use('local', new LocalStrategy(
-        function(username, password, done) {
-            // asynchronous verification, for effect...
-            process.nextTick(function() {
-
-                // Find the user by username.  If there is no user with the given
-                // username, or the password is not correct, set the user to `false` to
-                // indicate failure and set a flash message.  Otherwise, return the
-                // authenticated `user`.
-                users.findByUsername(username, function(err, user) {
-                    if (err) {
-                        return done(err);
-                    }
-                    if (!user) {
-                        return done(null, false, {
-                            message: 'Unknown user ' + username
-                        });
-                    }
-                    pass.hash(password, user.salt, function(err, hash) {
-                        console.log("sub hash: " + hash);
-                        console.log("usr hash: " + user.password);
-                        if(err) { 
-                            return done(err); 
-                        }
-                        if (user.password == hash) {
-                            done(null, user);
-                        }
-                        else {
-                            return done(null, false, {
-                                message: 'Invalid password'
-                            });
-                        }
-                    });
-                })
-            });
-        }
-    ));
-}
 
 function setupStaticRouting(app) {
     app.use(express.static(__dirname + '/public'));
@@ -92,7 +39,6 @@ function setupViewEngine(app) {
 function setupLogging(app) {
     app.use(morgan('dev'));
 }
-
 
 function setupStylus(app) {
     app.use(stylus.middleware({
@@ -117,9 +63,7 @@ function setupAuthenticationMiddleware(app) {
         saveUninitialized: false
     }));
     
-    initPassport();
-    app.use(passport.initialize());
-    app.use(passport.session());
+    auth.initialize(app);
 }
 
 function createApp() {
