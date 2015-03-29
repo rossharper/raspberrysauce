bf79635bfdd8fba524284430f7e3c7c512691099
rossharper <ross@rossharper.net>
2015-03-29 22:55:43 +0100

    breaking out local strategy for auth

--- webapp/auth/Authentication.js
+++ webapp/auth/Authentication.js
@@ -1,55 +1,14 @@
-var passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy,
-    pass = require('pwd');
+var passport = require('passport');
 
-var users = require('../models/users');
-
-function createLocalStrategy() {
-    return new LocalStrategy(
-        function(username, password, done) {
-            // asynchronous verification, for effect...
-            process.nextTick(function() {
-
-                // Find the user by username.  If there is no user with the given
-                // username, or the password is not correct, set the user to `false` to
-                // indicate failure and set a flash message.  Otherwise, return the
-                // authenticated `user`.
-                users.findByUsername(username, function(err, user) {
-                    if (err) {
-                        return done(err);
-                    }
-                    if (!user) {
-                        return done(null, false, {
-                            message: 'Unknown user ' + username
-                        });
-                    }
-                    pass.hash(password, user.salt, function(err, hash) {
-                        console.log("sub hash: " + hash);
-                        console.log("usr hash: " + user.password);
-                        if(err) { 
-                            return done(err); 
-                        }
-                        if (user.password == hash) {
-                            done(null, user);
-                        }
-                        else {
-                            return done(null, false, {
-                                message: 'Invalid password'
-                            });
-                        }
-                    });
-                })
-            });
-        }
-    )
-}
+var users = require('../models/users'),
+    localStrategy = require('./fileLocalStrategy');
 
 function initPassport() {
     passport.serializeUser(users.serializeUser);
 
     passport.deserializeUser(users.deserializeUser);
 
-    passport.use('local', createLocalStrategy());
+    passport.use('local', localStrategy);
 }
 
 var auth = {
