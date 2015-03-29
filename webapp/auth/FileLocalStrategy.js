cdd6824ce53194cdc215733bcf933306932cd0bc
Ross Harper <rossharper@users.noreply.github.com>
2015-03-29 22:59:37 +0100

    removed redundant comments

--- webapp/auth/FileLocalStrategy.js
+++ webapp/auth/FileLocalStrategy.js
@@ -5,13 +5,9 @@ var users = require('../models/users');
 
 var fileLocalStrategy = new LocalStrategy(
     function(username, password, done) {
-        // asynchronous verification, for effect...
+        
         process.nextTick(function() {
 
-            // Find the user by username.  If there is no user with the given
-            // username, or the password is not correct, set the user to `false` to
-            // indicate failure and set a flash message.  Otherwise, return the
-            // authenticated `user`.
             users.findByUsername(username, function(err, user) {
                 if (err) {
                     return done(err);
