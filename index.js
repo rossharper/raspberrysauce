d1d7c7faf58f88748dc272ae26e1b9f54e800b2d
Ross Harper <rossharper@users.noreply.github.com>
2015-03-29 15:56:10 +0100

    broke out web app from main and reorganised

--- index.js
+++ index.js
@@ -1,86 +1,12 @@
-/*
-TODO: split this shite up
-*/
-
-/*
- * Module dependencies
- */
-var express = require('express'),
-    morgan = require('morgan'),
-    stylus = require('stylus'),
-    nib = require('nib'),
-    cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser');
-
-/*
- * Submodule dependencies
- */
-var auth = require('./auth/Authentication');
-    routes = require('./routes/index'),
-    secureServer = require('./servers/SecureServer');
+var app = require('./webapp/app'),
+    secureServer = require('./servers/SecureServer'),
     inSecureRedirect = require('./servers/RedirectingInsecureServer');
 
 var securePort = process.argv[2] || 4443,
     insecurePort = process.argv[3] || 8080;
 
-function setupStaticRouting(app) {
-    app.use(express.static(__dirname + '/public'));
-}
-
-function setupDynamicRouting(app) {
-    app.use('/', routes);
-}
-
-function setupViewEngine(app) {
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
-}
-
-function setupLogging(app) {
-    app.use(morgan('dev'));
-}
-
-function setupStylus(app) {
-    app.use(stylus.middleware({
-        src: __dirname + '/public',
-        compile: function(str, path) {
-            return stylus(str)
-                .set('filename', path)
-                .use(nib())
-        }
-    }));
-}
-
-function setupAuthenticationMiddleware(app) {
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({
-        extended: false
-    }));
-    app.use(cookieParser());
-    app.use(require('express-session')({
-        secret: 'keyboard cat',
-        resave: false,
-        saveUninitialized: false
-    }));
-    
-    auth.initialize(app);
-}
-
-function createApp() {
-    var app = express();
-    
-    setupViewEngine(app);
-    setupLogging(app);
-    setupStylus(app);
-    setupAuthenticationMiddleware(app);
-    setupStaticRouting(app);
-    setupDynamicRouting(app); 
-
-    return app;
-}
-
 function start() {
-    secureServer.start(createApp(), securePort);
+    secureServer.start(app.create(), securePort);
     inSecureRedirect.start(insecurePort);
 }
 
