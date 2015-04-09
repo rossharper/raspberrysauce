2dc41c4420c67e3efc9567bddf66d39cfc086314
rossharper <ross@rossharper.net>
2015-04-09 20:06:22 +0100

    try shell command for boiler control instead of node plugin

--- webapp/api/boiler.js
+++ webapp/api/boiler.js
@@ -1,14 +1,24 @@
 var boilerControlLib = require('boilercontrol');
 
+var sys = require('sys');
+var exec = require('child_process').exec;
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+
 module.exports = {
     on: function(req, res) {
-        var boilercontrol = new boilerControlLib.BoilerControl(0);
-        boilercontrol.sendOnSignal();
+        //var boilercontrol = new boilerControlLib.BoilerControl(0);
+        //boilercontrol.sendOnSignal();
+
+        exec("callforheat 1", puts);
+
         res.send();
     },
     off: function(req, res) {
-        var boilercontrol = new boilerControlLib.BoilerControl(0);
-        boilercontrol.sendOffSignal();
+        //var boilercontrol = new boilerControlLib.BoilerControl(0);
+        //boilercontrol.sendOffSignal();
+
+        exec("callforheat 0", puts);
+
         res.send();
     }
 }
