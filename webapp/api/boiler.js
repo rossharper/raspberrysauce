93258c3523fd7efe1c059f1568e122678df35fc9
rossharper <ross@rossharper.net>
2015-04-09 21:41:18 +0100

    remove boilercontrol-node

--- webapp/api/boiler.js
+++ webapp/api/boiler.js
@@ -1,22 +1,14 @@
-var boilerControlLib = require('boilercontrol');
-
 var sys = require('sys');
 var exec = require('child_process').exec;
 function puts(error, stdout, stderr) { console.log(stdout) }
 
 module.exports = {
     on: function(req, res) {
-        //var boilercontrol = new boilerControlLib.BoilerControl(0);
-        //boilercontrol.sendOnSignal();
-
         exec("callforheat 1", puts);
 
         res.send();
     },
     off: function(req, res) {
-        //var boilercontrol = new boilerControlLib.BoilerControl(0);
-        //boilercontrol.sendOffSignal();
-
         exec("callforheat 0", puts);
 
         res.send();
