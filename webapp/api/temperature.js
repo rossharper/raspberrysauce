161cd5fabf1db650988f0fb2e51da6cdaa260cb4
Ross Harper <rossharper@users.noreply.github.com>
2015-04-11 12:06:20 +0100

    temperature tile styling

--- webapp/api/temperature.js
+++ webapp/api/temperature.js
@@ -2,7 +2,7 @@ var Temperature = require('../models/temperature');
 
 module.exports = {
     getCurrent: function(req, res) {
-        var randomTemp = (Math.random() * (21.0 - 4.0) + 4.0).toFixed(1);
+        var randomTemp = (Math.random() * 30.0).toFixed(1);
         var temp = new Temperature(randomTemp, new Date());
         res.send(temp);
     }
