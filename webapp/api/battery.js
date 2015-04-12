925b9a656569ec7666858df46944042bfc20b837
Ross Harper <rossharper@users.noreply.github.com>
2015-04-12 12:29:22 +0100

    find all batteryvalues

--- webapp/api/battery.js
+++ webapp/api/battery.js
@@ -2,7 +2,7 @@ var Battery = require('../models/battery');
 
 module.exports = {
     getCurrentVoltage: function(req, res) {
-        Battery.findOne().sort({date: -1}).limit(1).exec(function(err, battery) {
+        Battery.find().sort({date: -1}).limit(1).exec(function(err, battery) {
             console.log(err);
             console.log(battery);
             if(battery && battery.length > 0) {
