a36f637297309805f39977b1b99cab388c771d94
Ross Harper <rossharper@users.noreply.github.com>
2015-04-12 11:48:49 +0100

    adding battery reading (not working for some reason)

--- webapp/api/temperature.js
+++ webapp/api/temperature.js
@@ -1,7 +1,7 @@
 var Temperature = require('../models/temperature');
 
 module.exports = {
-    getCurrent: function(req, res) {
+    getCurrentTemperature: function(req, res) {
         Temperature.find().sort({date: -1}).limit(1).exec(function(err, temperature) {
             console.log(err);
             console.log(temperature);
