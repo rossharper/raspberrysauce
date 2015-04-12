4b24e28ac7c9164b686c0cbe8066e0e26a84e1b5
rossharper <ross@rossharper.net>
2015-04-12 15:53:54 +0100

    try catch in redirecting server

--- webapp/api/temperature.js
+++ webapp/api/temperature.js
@@ -3,8 +3,6 @@ var Temperature = require('../models/temperature');
 module.exports = {
     getCurrentTemperature: function(req, res) {
         Temperature.find().sort({date: -1}).limit(1).exec(function(err, temperature) {
-            console.log(err);
-            console.log(temperature);
             if(temperature && temperature.length > 0) {
                 res.send(temperature[0]);
             } else {
