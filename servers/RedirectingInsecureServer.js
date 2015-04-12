e1888a455b2a2d4c51ba6c577c3e20010df4edfc
Ross Harper <rossharper@users.noreply.github.com>
2015-04-12 15:53:54 +0100

    try catch in redirecting server

--- servers/RedirectingInsecureServer.js
+++ servers/RedirectingInsecureServer.js
@@ -3,12 +3,17 @@ var http = require('http');
 function configureInsecureTrafficRedirect(inSecurePort) {
     var insecureServer = http.createServer();
     insecureServer.on('request', function(req, res) {
-        // TODO also redirect websocket upgrades
-        res.setHeader(
-            'Location', 'https://' + req.headers.host.replace(/:\d+/, '') + req.url
-        );
-        res.statusCode = 302;
-        res.end();
+        try {
+            res.setHeader(
+                'Location', 'https://' + req.headers.host.replace(/:\d+/, '') + req.url
+            );
+            res.statusCode = 302;
+            res.end();
+        } catch (err) {
+            console.log("ERROR: " + err + "\nwhile serving request " + req);
+            res.statusCode = 500;
+            res.end();
+        }
     });
     insecureServer.listen(inSecurePort, function() {
         console.log("\nRedirecting all http traffic to https\n");
--- webapp/api/battery.js
+++ webapp/api/battery.js
@@ -3,8 +3,6 @@ var Battery = require('../models/battery');
 module.exports = {
     getCurrentVoltage: function(req, res) {
         Battery.find().sort({date: -1}).limit(1).exec(function(err, battery) {
-            console.log(err);
-            console.log(battery);
             if(battery && battery.length > 0) {
                 res.send(battery[0]);
             } else {
