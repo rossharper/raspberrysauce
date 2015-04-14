26bcc7144690bb49bf99bed24ff671b575cf9cbf
Ross Harper <rossharper@users.noreply.github.com>
2015-04-14 21:17:25 +0100

    chart formatting

--- webapp/public/js/charts.js
+++ webapp/public/js/charts.js
@@ -11,10 +11,42 @@ function generateChart(data, chart, value, xAxisValues) {
         axis: {
             x: {
                 tick: {
-                    fit: false
+                    fit: false,
+                    format: function(d) {
+                        var matches = data[d].date.match(/\d{4}-\d{2}-\d{2}T(\d{2}:\d{2}).*/);
+                        return matches[1];
+                    }
                 },
                 type: 'category'
             }
+        },
+        grid: {
+            x: {
+                show: true
+            },
+            y: {
+                show: true
+            }
+        },
+        legend: {
+            show: false
+        },
+        point: {
+            show: false
+        },
+        tooltip: {
+            format: {
+                title: function (d) {
+                    var matches = data[d].date.match(/\d{4}-\d{2}-\d{2}T(\d{2}:\d{2}).*/);
+                    return 'Time: ' + matches[1];
+                },
+                value: function (value, ratio, id) {
+                    return parseFloat(value).toFixed(2) + "&deg;C";
+                }
+            }
+        },
+        zoom: {
+            enabled: true
         }
     });
 }
