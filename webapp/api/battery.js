4f962607066da2d19591c9ee3c59d0497fd7540c
Ross Harper <rossharper@users.noreply.github.com>
2015-11-07 21:44:07 +0000

    function for getting batt val from db

--- webapp/api/battery.js
+++ webapp/api/battery.js
@@ -1,14 +1,18 @@
 var Battery = require('../models/battery');
 
+function getLatestVoltageFromDatabase(callback) {
+    Battery.find().sort({date: -1}).limit(1).exec(function(err, battery) {
+        if(battery && battery.length > 0) {
+            callback(battery[0]);
+        } else {
+            callback({});
+        }
+    });
+}
+
 module.exports = {
     getCurrentVoltage: function(req, res) {
-        Battery.find().sort({date: -1}).limit(1).exec(function(err, battery) {
-            if(battery && battery.length > 0) {
-                res.send(battery[0]);
-            } else {
-                res.send({});
-            }
-        });
+        getLatestVoltageFromDatabase(function(voltage) { res.send(voltage) });
     },
     getHistory: function(req, res) {
         Battery.find().sort({date: 1}).exec(function(err, voltages) {
