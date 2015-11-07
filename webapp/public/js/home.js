919df78271fc3c9b3ace1958bbdd47188e55a608
Ross Harper <ross.harper@bbc.co.uk>
2015-11-07 23:11:13 +0000

    hooking view api into homepage

---
+++ webapp/public/js/home.js
@@ -0,0 +1,39 @@
+function displayTemperature(temperature) {
+    var displayTemperature = roundToOneDecimalPlace(temperature);
+    document.getElementById("currentTemperature").innerHTML = displayTemperature + "&deg;C";
+    var ranges = [0,1,4,7,10,13,16,19,22,25];
+    var i = 0;
+    while(i < ranges.length) {
+        if(displayTemperature < ranges[i]) break;
+        ++i;
+    }
+    var tempClass = ranges[Math.max(i-1, 0)];
+    document.getElementById("temperaturetile").title = tempClass;
+}
+
+function displayBattery(batteryVoltage) {
+    document.getElementById("currentBattery").innerHTML = roundToOneDecimalPlace(batteryVoltage) + "V";
+}
+
+function displayView(viewData) {
+    displayTemperature(viewData.temperature);
+    displayBattery(viewData.batteryVoltage);
+}
+
+function scheduleReload() {
+    window.setTimeout(loadView, 60000);
+}
+function loadView() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if(xhr.status === 200) {
+                var jsonResponse = JSON.parse(xhr.responseText);
+                displayView(jsonResponse);
+            }
+            scheduleReload();
+        }
+    }
+    xhr.open('GET', 'api/views/home', true);
+    xhr.send(null);
+}
