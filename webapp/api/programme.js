4327daaf6103b1b07a5d14cf0a3dcd80f6243e65
Ross Harper <ross@rossharper.net>
2015-11-27 23:15:32 +0000

    Adding api to turn back to auto mode

--- webapp/api/programme.js
+++ webapp/api/programme.js
@@ -4,17 +4,30 @@ var ProgrammeFileWriter = require('heatingprogramme').ProgrammeFileWriter;
 // TODO: inject!
 var PROGRAMME_DATA_PATH = "/var/lib/homecontrol/programdata";
 
+function writeProgramme(programme, res) {
+    ProgrammeFileWriter.writeProgramme(PROGRAMME_DATA_PATH, programme, function(err) {
+        if(err) {
+            res.send("Write failed: " + err);
+            return;
+        }
+        res.send("OK");
+    });
+}
+
 module.exports = {
+
+    setHeatingModeAuto : function(req, res) {
+        ProgrammeFileLoader.loadProgramme(PROGRAMME_DATA_PATH, function(programme) {
+            programme.setHeatingEnabled(true);
+            programme.clearOverride();
+            writeProgramme(programme, res);
+        })
+    },
+
     setHeatingModeOff : function(req, res) {
         ProgrammeFileLoader.loadProgramme(PROGRAMME_DATA_PATH, function(programme) {
             programme.setHeatingEnabled(false);
-            ProgrammeFileWriter.writeProgramme(PROGRAMME_DATA_PATH, programme, function(err) {
-                if(err) {
-                    res.send("Write failed: " + err);
-                    return;
-                }
-                res.send("OK");
-            });
+            writeProgramme(programme, res);
         });
     }
 }
