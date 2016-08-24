859e79c2af01b23c5c83fb3cc3f1a5b388714bfd
Ross Harper <ross.harper@bbc.co.uk>
2016-08-24 14:44:02 +0100

    add user to file

--- webapp/auth/userRepository.js
+++ webapp/auth/userRepository.js
@@ -1,6 +1,13 @@
+'use strict';
+
+const jsonfile = require('jsonfile');
+
+const PATH = '/var/lib/homecontrol/webapp/users/';
+
 module.exports = {
   addUser: function (user, cb) {
-    // TODO: implement
-    cb(new Error('impl'));
+    jsonfile.writeFile(PATH + user.username, user, (err) => {
+      cb(err);
+    });
   }
 };
