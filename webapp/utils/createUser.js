887d7b6d15330987899acde578114ba7c2350617
Ross Harper <rossharper@users.noreply.github.com>
2016-08-24 13:43:52 +0100

    Delinting user stuff before implementing file based users

--- webapp/utils/createUser.js
+++ webapp/utils/createUser.js
@@ -1,60 +1,46 @@
-var User = require('./../models/user'),
-    mongoose = require('mongoose'),
-    pass = require('pwd');
+'use strict';
 
-var dbConfig = require('./../db/db.js');
+const User = require('./../models/user');
+const pass = require('pwd');
 
 function addUser(username, salt, password, email) {
-    mongoose.connect(dbConfig.url);
-
-    var newUser = new User();
+    const newUser = new User();
 
     newUser.username = username;
     newUser.salt = salt;
     newUser.password = password;
     newUser.email = email;
 
-    newUser.save(function (err) {
-        if (err) {
-            console.log('Error in Saving user: ' + err);
-            throw err;
-        }
-        console.log('User Registration succesful');
-        return done(null, newUser);
-    });
+    // TODO: save user
 }
 
 function createUser(username, password, email) {
-    pass.hash(password, function(err, salt, passhash) {
-        if(err) {
+    pass.hash(password, (err, salt, passhash) => {
+        if (err) {
             console.log(err);
-        }
-        else
-        {
+        } else {
             addUser(username, salt, passhash, email);
         }
     });
 }
 
-var argv = process.argv;
-
-var username;
-if(process.argv.indexOf("-u") != -1){
-    username = process.argv[process.argv.indexOf("-u") + 1];
+let username;
+if (process.argv.indexOf('-u') !== -1) {
+    username = process.argv[process.argv.indexOf('-u') + 1];
 }
 
-var password;
-if(process.argv.indexOf("-p") != -1){
-    password = process.argv[process.argv.indexOf("-p") + 1];
+let password;
+if (process.argv.indexOf('-p') !== -1) {
+    password = process.argv[process.argv.indexOf('-p') + 1];
 }
 
-var email;
-if(process.argv.indexOf("-e") != -1){
-    email = process.argv[process.argv.indexOf("-e") + 1];
+let email;
+if (process.argv.indexOf('-e') !== -1) {
+    email = process.argv[process.argv.indexOf('-e') + 1];
 }
 
-if(!username || !password || !email) {
-    console.log("Usage: node createUser.js -u <username> -p <password> -e <email>");
+if (!username || !password || !email) {
+    console.log('Usage: node createUser.js -u <username> -p <password> -e <email>');
 } else {
     createUser(username, password, email);
 }
