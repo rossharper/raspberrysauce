5413737c15d0f6098f7d6c73cfb58e89e2c6c087
Ross Harper <ross.harper@bbc.co.uk>
2016-08-24 11:51:41 +0100

    Add eslint, start removing db

--- webapp/models/user.js
+++ webapp/models/user.js
@@ -1,8 +1,3 @@
-var mongoose = require('mongoose');
 
-module.exports = mongoose.model('User',{
-    username: String,
-    salt: String,
-    password: String,
-    email: String
-});
+
+module.exports = new Error('impl');
