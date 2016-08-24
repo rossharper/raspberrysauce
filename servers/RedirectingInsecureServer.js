cfa0a57bb33cd908c33b329fad4418d45f99ca8b
Ross Harper <ross.harper@bbc.co.uk>
2016-08-24 13:35:55 +0100

    Delinting server code

--- servers/RedirectingInsecureServer.js
+++ servers/RedirectingInsecureServer.js
@@ -1,8 +1,10 @@
-var http = require('http');
+'use strict';
+
+const http = require('http');
 
 function configureInsecureTrafficRedirect(inSecurePort) {
-    var insecureServer = http.createServer();
-    insecureServer.on('request', function(req, res) {
+    const insecureServer = http.createServer();
+    insecureServer.on('request', (req, res) => {
         try {
             res.setHeader(
                 'Location', 'https://' + req.headers.host.replace(/:\d+/, '') + req.url
@@ -10,18 +12,18 @@ function configureInsecureTrafficRedirect(inSecurePort) {
             res.statusCode = 302;
             res.end();
         } catch (err) {
-            console.log("ERROR: " + err + "\nwhile serving request " + req);
+            console.log('ERROR: ' + err + '\nwhile serving request ' + req);
             res.statusCode = 500;
             res.end();
         }
     });
-    insecureServer.listen(inSecurePort, function() {
-        console.log("\nRedirecting all http traffic to https\n");
+    insecureServer.listen(inSecurePort, () => {
+        console.log('\nRedirecting all http traffic to https\n');
     });
 }
 
 module.exports = {
-    start: function(inSecurePort) {
+    start: function (inSecurePort) {
         configureInsecureTrafficRedirect(inSecurePort);
     }
 };
