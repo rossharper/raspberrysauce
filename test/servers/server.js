317fa0d86f45074092473697f0ae516c71342666
Ross Harper <ross.harper@bbc.co.uk>
2016-08-25 17:51:46 +0100

    Allow server to be started on specified port

--- test/servers/server.js
+++ test/servers/server.js
@@ -5,19 +5,44 @@ const http = require('http');
 const assert = require('assert');
 const server = require('../../servers/server');
 
+var sandbox = sinon.sandbox.create();
+
 describe('server', () => {
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
   it('creates an HTTP server on port 8080 with supplied application by default', (done) => {
     const app = {};
     const mockServer = {
-      listen: sinon.spy()
+      listen: sandbox.spy()
     };
 
-    sinon.stub(http, 'createServer').returns(mockServer);
+    sandbox.stub(http, 'createServer').returns(mockServer);
 
     server.start(app);
 
-    assert(http.createServer.calledWith(app));
-    assert(mockServer.listen.calledWith(8080));
+    sinon.assert.calledWith(http.createServer, sandbox.match(app));
+    sinon.assert.calledWith(mockServer.listen, sinon.match(8080));
     done();
   });
+
+  it('creates an HTTP server on specified port', (done) => {
+    const app = {};
+    const mockServer = {
+      listen: sandbox.spy()
+    };
+
+    sandbox.stub(http, 'createServer').returns(mockServer);
+
+    const port = 6969;
+    server.start(app, {
+      port: port
+    });
+
+    sinon.assert.calledWith(http.createServer, sandbox.match(app));
+    sinon.assert.calledWith(mockServer.listen, sandbox.match(port));
+    done();
+  })
 });
