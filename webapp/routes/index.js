8876f025611f831a0b192515dda3992f6e05fe60
Ross Harper <rossharper@users.noreply.github.com>
2016-08-25 10:39:32 +0100

    Extract schedule api routes

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 
 var
-    scheduleapi = require('../api/schedule'),
+
 
     homeviewapi = require('../api/views/home');
 
@@ -11,8 +11,7 @@ function initRoutes() {
 
 
 
-    router.get('/api/schedule/all', scheduleapi.getSchedules);
-    router.post('/api/schedule/add', scheduleapi.addSchedule);
+
 
     router.get('/api/views/home', homeviewapi.getView);
 }
