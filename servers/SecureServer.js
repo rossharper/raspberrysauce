e3a18ce13b382a77e284f47f81d19327eb1e36ad
Ross Harper <rossharper@users.noreply.github.com>
2016-08-25 21:37:18 +0100

    create a secure server with options

--- servers/SecureServer.js
+++ servers/SecureServer.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const https = require('https');
-const sslrootcas = require('ssl-root-cas');
 const fs = require('fs');
 const path = require('path');
 const _ = require('lodash');
@@ -11,29 +10,6 @@ const DEFAULT_TLS_PATH = path.join(process.cwd(), 'certs');
 const DEFAULT_CERT_PATH = path.join(DEFAULT_TLS_PATH, 'servercert.pem');
 const DEFAULT_KEY_PATH = path.join(DEFAULT_TLS_PATH, 'serverkey.pem');
 const DEFAULT_PASSPHRASE = '';
-
-/*
- Config file - don't store in repo
-
- Format:
-
- sslconfig = {
-    passphrase = "certificatepassword",
-    cacertpath = process.env['HOME'] + "/path/to/cacerts",
-    cacert = "ca_cert_name.pem",
-    servercertpath = process.env['HOME'] + "/path/tp/servercert";
-    servercert = "server.crt.pem";
-    serverkey = "server.key.pem"
- };
-
-*/
-const sslConfig = require('./config/sslconfig');
-
-function configureRootCerts() {
-  sslrootcas
-    .inject()
-    .addFile(path.join(sslConfig.cacertpath, sslConfig.cacert));
-}
 
 function createTlsOptions(opts) {
   const options = {
@@ -47,7 +23,6 @@ function createTlsOptions(opts) {
 function configureSslServer(app, opts) {
   const port = _.get(opts, 'port', DEFAULT_PORT);
 
-  configureRootCerts();
   const tlsOpts = createTlsOptions(opts);
 
   const server = https.createServer(tlsOpts, app).listen(port, () => {
--- test/servers/server.js
+++ test/servers/server.js
@@ -7,12 +7,20 @@ const server = require('../../servers/server');
 const path = require('path');
 const fs = require('fs');
 
+const CERT_FIXTURE_PATH = path.join(__dirname, 'fixtures', 'servercert.pem');
+const KEY_FIXTURE_PATH = path.join(__dirname, 'fixtures', 'serverkey.pem');
+
 const sandbox = sinon.sandbox.create();
 
 const app = {};
 const mockServer = {
   listen: sandbox.spy()
 };
+
+function stubFsForDefaultCerts() {
+  sandbox.stub(fs, 'readFileSync').withArgs(path.join(process.cwd(), 'certs', 'serverkey.pem')).returns('defaultkey');
+  fs.readFileSync.withArgs(path.join(process.cwd(), 'certs', 'servercert.pem')).returns('defaultcert');
+}
 
 describe('server', () => {
 
@@ -49,14 +57,13 @@ describe('server', () => {
 
   describe('secured server', () => {
 
-    it.only('creates an HTTPS server on port 4443 with supplied application and defaults', (done) => {
+    it('creates an HTTPS server on port 4443 with supplied application and defaults', (done) => {
       const expectedOptions = {
-        key: 'keyfixture',
-        cert: 'certfixture',
+        key: 'defaultkey',
+        cert: 'defaultcert',
         passphrase: ''
       };
-      sandbox.stub(fs, 'readFileSync').withArgs(path.join(process.cwd(), 'certs', 'serverkey.pem')).returns('keyfixture');
-      fs.readFileSync.withArgs(path.join(process.cwd(), 'certs', 'servercert.pem')).returns('certfixture');
+      stubFsForDefaultCerts();
 
       server.start(app, {
         securedServer: {
@@ -69,7 +76,41 @@ describe('server', () => {
       done();
     });
 
-    // TODO: it should start a secure server on the specified port when the options say so
+    it('creates an HTTPS server on the specified port', (done) => {
+      stubFsForDefaultCerts();
+
+      server.start(app, {
+        securedServer: {
+          enabled: true,
+          port: 6969
+        }
+      });
+
+      sinon.assert.calledWith(https.createServer, sandbox.match(app));
+      sinon.assert.calledWith(mockServer.listen, sinon.match(6969));
+      done();
+    });
+
+    it('creates an HTTPS server with specified cert, key and passphrase', (done) => {
+      const expectedOptions = {
+        cert: fs.readFileSync(CERT_FIXTURE_PATH),
+        key: fs.readFileSync(KEY_FIXTURE_PATH),
+        passphrase: 'passphrase'
+      };
+
+      server.start(app, {
+        securedServer: {
+          enabled: true,
+          serverCertPath: CERT_FIXTURE_PATH,
+          serverKeyPath: KEY_FIXTURE_PATH,
+          passphrase: 'passphrase'
+        }
+      });
+
+      sinon.assert.calledWith(https.createServer, sandbox.match(expectedOptions), sandbox.match(app));
+      done();
+    });
+
     // TODO: tests around using the certs
     // TODO: allow passthru tls options
     // TODO: path cert options override tls options
