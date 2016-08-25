b3bbc07a0ed3ca9808b575896748f62eca053c9b
Ross Harper <rossharper@users.noreply.github.com>
2016-08-25 22:12:55 +0100

    Configure CA certs

--- servers/SecureServer.js
+++ servers/SecureServer.js
@@ -4,6 +4,7 @@ const https = require('https');
 const fs = require('fs');
 const path = require('path');
 const _ = require('lodash');
+const FileHound = require('filehound');
 
 const DEFAULT_PORT = 4443;
 const DEFAULT_TLS_PATH = path.join(process.cwd(), 'certs');
@@ -11,12 +12,23 @@ const DEFAULT_CERT_PATH = path.join(DEFAULT_TLS_PATH, 'servercert.pem');
 const DEFAULT_KEY_PATH = path.join(DEFAULT_TLS_PATH, 'serverkey.pem');
 const DEFAULT_PASSPHRASE = '';
 
+function loadCaCerts(caPath) {
+  return FileHound.create()
+    .paths(caPath)
+    .ext('pem')
+    .findSync()
+    .map((caCertPath) => {
+      return fs.readFileSync(caCertPath);
+    });
+}
+
 function createTlsOptions(opts) {
   const options = {
     key: fs.readFileSync(opts.serverKeyPath || DEFAULT_KEY_PATH),
     cert: fs.readFileSync(opts.serverCertPath || DEFAULT_CERT_PATH),
     passphrase: opts.passphrase || DEFAULT_PASSPHRASE
   };
+  if (opts.caPath) options.ca = loadCaCerts(opts.caPath);
   return options;
 }
 
