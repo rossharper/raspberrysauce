b11df915c217f14b26607f7bcc0d909bdec05168
Ross Harper <ross.harper@bbc.co.uk>
2016-08-25 09:58:10 +0100

    Split out auth routing

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express')
 
-var auth = require('../auth/Authentication'),
-    temperatureapi = require('../api/temperature'),
+var temperatureapi = require('../api/temperature'),
     batteryapi = require('../api/battery'),
     scheduleapi = require('../api/schedule'),
     programmeapi = require('../api/programme'),
@@ -9,50 +8,7 @@ var auth = require('../auth/Authentication'),
 
 var router = express.Router();
 
-function ensureAuthenticatedPage(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect('/login')
-}
-
-function ensureAuthenticatedApiCall(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.status(401).send("401 Not Authorized");
-}
-
 function initRoutes() {
-    router.get('/login', function(req, res) {
-        res.render('login', {
-            title: 'Login',
-            message: req.flash('message')
-        })
-    });
-
-    var authenticationRedirects = {
-        successRedirect: '/',
-        failureRedirect: '/login',
-        failureFlash: true
-    };
-    router.post('/login', auth.getAuthenticationHandler(authenticationRedirects));
-
-    router.get('/logout', function(req, res) {
-        req.logout();
-        res.redirect('/login');
-    });
-
-    router.all('*', function(req, res, next) {
-        if (req.params === '/login') {
-            next();
-        } else if (req.params[0].lastIndexOf("/api/") === 0) {
-            ensureAuthenticatedApiCall(req, res, next);
-        }
-        else {
-            ensureAuthenticatedPage(req, res, next);
-        }
-    });
 
     router.get('/', function(req, res) {
         res.render('index', {
