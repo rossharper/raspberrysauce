8cc1616d3e8fbde072c2724f407860a55963b82a
Ross Harper <ross.harper@bbc.co.uk>
2016-08-26 16:58:49 +0100

    tests around server API

--- servers/insecureServer.js
+++ servers/insecureServer.js
@@ -7,10 +7,16 @@ const DEFAULT_PORT = 8080;
 
 module.exports = {
   start: function (app, opts) {
-    const port = _.get(opts, "port", DEFAULT_PORT);
-    const server = http.createServer(app).listen(port, () => {
+    const port = _.get(opts, 'port', DEFAULT_PORT);
+    const server = http.createServer(app);
+    server.listen(port, () => {
       const listeningPort = server.address().port;
       console.log('Listening on http://' + server.address().address + ':' + listeningPort);
     });
+    return {
+      close: function () {
+        server.close();
+      }
+    };
   }
 };
--- servers/server.js
+++ servers/server.js
@@ -3,14 +3,7 @@
 const _ = require('lodash');
 const unsecuredServer = require('./insecureServer');
 const securedServer = require('./SecureServer');
-
-function startUnsecuredServer(app, opts) {
-  unsecuredServer.start(app, opts);
-}
-
-function startSecuredServer(app, opts) {
-  securedServer.start(app, opts);
-}
+const redirectingServer = require('./RedirectingInsecureServer');
 
 function getUnsecuredServerConfig(opts) {
   return _.get(opts, 'unsecuredServer', {
@@ -28,12 +21,32 @@ function start(app, opts) {
   const unsecuredServerConfig = getUnsecuredServerConfig(opts);
   const secureServerConfig = getSecureServerConfig(opts);
 
+  let unsecuredServerInstance;
+  let securedServerInstance;
+  let redirectingServerInstance;
+
   if (secureServerConfig.enabled) {
-    startSecuredServer(app, secureServerConfig);
+    securedServerInstance = securedServer.start(app, secureServerConfig, (port) => {
+      if (unsecuredServerConfig.enabled && unsecuredServerConfig.redirectsToSecuredServer) {
+        const redirectConfig = {
+          securedServerPort: port
+        };
+        if (unsecuredServerConfig.port) redirectConfig.unSecuredServerPort = unsecuredServerConfig.port;
+        redirectingServerInstance = redirectingServer.start(redirectConfig);
+      }
+    });
   }
-  if (unsecuredServerConfig.enabled) {
-    startUnsecuredServer(app, unsecuredServerConfig);
+  if (unsecuredServerConfig.enabled && !unsecuredServerConfig.redirectsToSecuredServer) {
+    unsecuredServerInstance = unsecuredServer.start(app, unsecuredServerConfig);
   }
+
+  return {
+    close: function () {
+      if (unsecuredServerInstance) unsecuredServerInstance.close();
+      if (securedServerInstance) securedServerInstance.close();
+      if (redirectingServerInstance) redirectingServerInstance.close();
+    }
+  };
 }
 
 module.exports = {
