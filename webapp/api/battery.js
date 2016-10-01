a73684790f0840328ed92d5adc8f0ee1cb216f6f
Ross Harper <ross@rossharper.net>
2016-10-01 11:09:09 +0100

    Removed unused database and graphs

--- webapp/api/battery.js
+++ webapp/api/battery.js
@@ -1,15 +1,4 @@
-var Battery = require('../models/battery');
 var BatteryFile = require('../models/batteryFile');
-
-function getLatestVoltageFromDatabase(callback) {
-    Battery.find().sort({date: -1}).limit(1).exec(function(err, battery) {
-        if(battery && battery.length > 0) {
-            callback(battery[0]);
-        } else {
-            callback({});
-        }
-    });
-}
 
 function getVoltageFromFile(callback) {
     BatteryFile.readFromFile(callback);
@@ -18,10 +7,5 @@ function getVoltageFromFile(callback) {
 module.exports = {
     getCurrentVoltage: function(req, res) {
         getVoltageFromFile(function(voltage) { res.send(voltage) });
-    },
-    getHistory: function(req, res) {
-        Battery.find().sort({date: 1}).exec(function(err, voltages) {
-            res.send(voltages);
-        });
     }
 }
--- webapp/routes/battery_api.js
+++ webapp/routes/battery_api.js
@@ -5,6 +5,5 @@ const batteryapi = require('../api/battery');
 const requiresApiAuthorization = require('../auth/requiresApiAuthorization');
 
 router.get('/api/battery/currentVoltage', requiresApiAuthorization(), batteryapi.getCurrentVoltage);
-router.get('/api/battery/history', requiresApiAuthorization(), batteryapi.getHistory);
 
 module.exports = router;
