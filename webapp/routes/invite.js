d791d2026581064ae1aa26b1efbb15f25359c416
Ross Harper <ross.harper@bbc.co.uk>
2016-10-28 13:53:38 +0100

    Add route for posting invite

--- webapp/routes/invite.js
+++ webapp/routes/invite.js
@@ -2,10 +2,37 @@
 
 const router = require('express').Router();
 const requiresAuthorizedUser = require('../auth/requiresAuthorizedUser');
+const inviteUser = require('../auth/inviteUser').inviteUser;
+const Joi = require('joi');
+
+var schema = Joi.object().keys({
+  email: Joi.string().email().required()
+});
 
 router.get('/invite', requiresAuthorizedUser(), (req, res) => {
   res.render('invite', {
-    title: 'Invite New User'
+    title: 'Invite New User',
+    error: req.flash('error'),
+    success: req.flash('success')
+  });
+});
+
+router.post('/invite', requiresAuthorizedUser(), (req, res) => {
+  Joi.validate(req.body, schema, (err, email) => {
+    if (err) {
+      req.flash('error', 'Please enter a valid email address');
+      res.redirect('/invite');
+      return;
+    } else {
+      inviteUser(email, (err) => {
+        if (err) {
+          req.flash('error', 'The email could not be sent.');
+        } else {
+          req.flash('success', 'An invitation has been sent to the user.');
+        }
+        res.redirect('/invite');
+      });
+    }
   });
 });
 
