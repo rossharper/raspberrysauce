b4ac4ec4407fa801e29d05b8d9d472b7d7604598
Ross Harper <rossharper@users.noreply.github.com>
2016-12-21 16:17:31 +0000

    Cache control for temperature API

--- webapp/api/temperature.js
+++ webapp/api/temperature.js
@@ -1,18 +1,28 @@
-var TemperatureFile = require('../models/temperatureFile');
+const TemperatureFile = require('../models/temperatureFile');
+
+const DEFAULT_TEMP_SAMPLE_INTERVAL = 120;
 
 function getTemperatureFromFile(callback) {
-    TemperatureFile.readFromFile(callback);
+  TemperatureFile.readFromFile(callback);
+}
+
+function tempMaxAge(temp) {
+  const tempSampleInterval = temp.sampleInterval || DEFAULT_TEMP_SAMPLE_INTERVAL;
+  const nextSampleTimestamp = new Date(temp.timestamp);
+  nextSampleTimestamp.setSeconds(nextSampleTimestamp.getSeconds() + tempSampleInterval);
+  return Math.max(0, Math.round((nextSampleTimestamp.getTime() - new Date().getTime()) / 1000));
 }
 
 module.exports = {
-    getCurrentTemperature: function(req, res) {
-        getTemperatureFromFile((err, temp) => {
-          if(err) {
-            res.status(500);
-            res.end();
-            return;
-          }
-          res.send(temp);
-        } );
-    }
+  getCurrentTemperature: function (req, res) {
+    getTemperatureFromFile((err, temp) => {
+      if (err) {
+        res.status(500);
+        res.end();
+        return;
+      }
+      res.set('Cache-Control', `private, max-age=${tempMaxAge(temp)}`)
+      res.send(temp);
+    });
+  }
 }
