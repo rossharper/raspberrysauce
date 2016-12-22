21780e5ca2fa34b117306271f38519da38b934d0
Ross Harper <rossharper@users.noreply.github.com>
2016-12-22 11:41:35 +0000

    Expose home view api with bearer auth for ios client

--- webapp/routes/home_view_api.js
+++ webapp/routes/home_view_api.js
@@ -3,7 +3,10 @@
 const router = require('express').Router();
 const homeviewapi = require('../api/views/home');
 const requiresApiAuthorization = require('../auth/requiresApiAuthorization');
+const auth = require('../auth/Authentication');
 
 router.get('/api/views/home', requiresApiAuthorization(), homeviewapi.getView);
+
+router.get('/api/views/ios/home', auth.getBearerHandler(), homeviewapi.getView);
 
 module.exports = router;
