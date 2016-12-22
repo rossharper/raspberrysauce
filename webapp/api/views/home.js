93e2bc4b8ba8635e79ec3c9652d3dde945eb7520
Ross Harper <ross@rossharper.net>
2016-12-22 16:37:34 +0000

    Allow programe mode changes with bearer token & POST

--- webapp/api/views/home.js
+++ webapp/api/views/home.js
@@ -1,44 +1,7 @@
 const batteryFile = require('../../models/batteryFile');
 const temperatureFile = require('../../models/temperatureFile');
 const programmeProvider = require('../../models/programmeProvider');
-const DateUtil = require('dateutil');
-
-function getTodaysSortedComfortPeriods(programme, date) {
-  const comfortPeriods = programme.getComfortPeriodsForDate(date);
-  comfortPeriods.sort((a, b) => {
-    const keyA = DateUtil.getDateFromTimeStr(date, a.startTime);
-    const keyB = DateUtil.getDateFromTimeStr(date, b.startTime);
-    if (keyA < keyB) return -1;
-    if (keyA > keyB) return 1;
-    return 0;
-  });
-  return comfortPeriods;
-}
-
-function addPeriod(periods, isComfort, startTime, endTime) {
-  periods.push({
-    'isComfort': isComfort,
-    'startTime': startTime,
-    'endTime': endTime
-  });
-}
-
-function getTodaysPeriods(programme, date) {
-  const comfortPeriods = getTodaysSortedComfortPeriods(programme, date);
-  const finalPeriods = [];
-  comfortPeriods.forEach((period) => {
-    if (finalPeriods.length === 0 && DateUtil.getDateFromTimeStr(date, period.startTime) > DateUtil.getDateFromTimeStr(date, '00:00')) {
-      addPeriod(finalPeriods, false, '00:00', period.startTime);
-    } else if (finalPeriods.length > 0 && DateUtil.getDateFromTimeStr(date, finalPeriods[finalPeriods.length - 1].endTime) < DateUtil.getDateFromTimeStr(date, period.startTime)) {
-      addPeriod(finalPeriods, false, finalPeriods[finalPeriods.length - 1].endTime, period.startTime);
-    }
-    addPeriod(finalPeriods, true, period.startTime, period.endTime);
-  });
-  if (DateUtil.getDateFromTimeStr(date, finalPeriods[finalPeriods.length - 1].endTime) < DateUtil.getDateFromTimeStr(date, '23:59')) {
-    addPeriod(finalPeriods, false, finalPeriods[finalPeriods.length - 1].endTime, '23:59');
-  }
-  return finalPeriods;
-}
+const programmeModelBuilder = require('../../models/programmeModelBuilder');
 
 module.exports = {
   getView: function (req, res, next) {
@@ -49,16 +12,10 @@ module.exports = {
           return;
         }
         programmeProvider.getProgramme((programme) => {
-          const now = new Date();
           const view = {
             temperature: temperature.temperature,
             batteryVoltage: voltage.batteryVoltage,
-            programme: {
-              heatingEnabled: programme.isHeatingEnabled(),
-              comfortLevelEnabled: programme.isInComfortMode(now),
-              inOverride: programme.isInOverridePeriod(now),
-              todaysPeriods: getTodaysPeriods(programme, now)
-            }
+            programme: programmeModelBuilder.buildFromProgramme(programme)
           };
           res.send(view);
         });
---
+++ webapp/models/programmeModelBuilder.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const DateUtil = require('dateutil');
+
+function getTodaysSortedComfortPeriods(programme, date) {
+  const comfortPeriods = programme.getComfortPeriodsForDate(date);
+  comfortPeriods.sort((a, b) => {
+    const keyA = DateUtil.getDateFromTimeStr(date, a.startTime);
+    const keyB = DateUtil.getDateFromTimeStr(date, b.startTime);
+    if (keyA < keyB) return -1;
+    if (keyA > keyB) return 1;
+    return 0;
+  });
+  return comfortPeriods;
+}
+
+function addPeriod(periods, isComfort, startTime, endTime) {
+  periods.push({
+    'isComfort': isComfort,
+    'startTime': startTime,
+    'endTime': endTime
+  });
+}
+
+function getTodaysPeriods(programme, date) {
+  const comfortPeriods = getTodaysSortedComfortPeriods(programme, date);
+  const finalPeriods = [];
+  comfortPeriods.forEach((period) => {
+    if (finalPeriods.length === 0 && DateUtil.getDateFromTimeStr(date, period.startTime) > DateUtil.getDateFromTimeStr(date, '00:00')) {
+      addPeriod(finalPeriods, false, '00:00', period.startTime);
+    } else if (finalPeriods.length > 0 && DateUtil.getDateFromTimeStr(date, finalPeriods[finalPeriods.length - 1].endTime) < DateUtil.getDateFromTimeStr(date, period.startTime)) {
+      addPeriod(finalPeriods, false, finalPeriods[finalPeriods.length - 1].endTime, period.startTime);
+    }
+    addPeriod(finalPeriods, true, period.startTime, period.endTime);
+  });
+  if (DateUtil.getDateFromTimeStr(date, finalPeriods[finalPeriods.length - 1].endTime) < DateUtil.getDateFromTimeStr(date, '23:59')) {
+    addPeriod(finalPeriods, false, finalPeriods[finalPeriods.length - 1].endTime, '23:59');
+  }
+  return finalPeriods;
+}
+
+module.exports = {
+  buildFromProgramme: function (programme) {
+    const now = new Date();
+    return {
+      heatingEnabled: programme.isHeatingEnabled(),
+      comfortLevelEnabled: programme.isInComfortMode(now),
+      inOverride: programme.isInOverridePeriod(now),
+      todaysPeriods: getTodaysPeriods(programme, now)
+    }
+  }
+}
