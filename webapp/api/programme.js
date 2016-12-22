8ed377203733c59fbde24c3cb9407c2eddaaeb65
Ross Harper <rossharper@users.noreply.github.com>
2016-12-22 16:37:34 +0000

    Allow programe mode changes with bearer token & POST

--- webapp/api/programme.js
+++ webapp/api/programme.js
@@ -1,92 +1,91 @@
 var ProgrammeFileLoader = require('heatingprogramme').ProgrammeFileLoader;
 var ProgrammeFileWriter = require('heatingprogramme').ProgrammeFileWriter;
-
+var programmeModelBuilder = require('../models/programmeModelBuilder')
 var programmeProvider = require('../models/programmeProvider');
 
 // TODO: inject!
 var PROGRAMME_DATA_PATH = "/var/lib/homecontrol/programdata";
 
-function writeProgramme(programme, successMessage, res) {
-    ProgrammeFileWriter.writeProgramme(PROGRAMME_DATA_PATH, programme, function(err) {
-        if(err) {
-            res.send("Write failed: " + err);
-            return;
-        }
-        res.send(successMessage);
-    });
+function writeProgramme(programme, res) {
+  ProgrammeFileWriter.writeProgramme(PROGRAMME_DATA_PATH, programme, function (err) {
+    if (err) {
+      res.status(500);
+      res.end();
+      return;
+    }
+    res.send(programmeModelBuilder.buildFromProgramme(programme));
+  });
 }
 
 function comfortUntilDate(untilDate, res) {
-    programmeProvider.getProgramme(function(programme) {
-        programme.setComfortOverride(untilDate);
-        writeProgramme(programme, "OK. COMFORT mode set until: " + untilDate.toISOString(), res)
-    });
+  programmeProvider.getProgramme(function (programme) {
+    programme.setComfortOverride(untilDate);
+    writeProgramme(programme, res)
+  });
 }
 
 function setbackUntilDate(untilDate, res) {
-    programmeProvider.getProgramme(function(programme) {
-        programme.setSetbackOverride(untilDate);
-        writeProgramme(programme, "OK. SETBACK mode set until: " + untilDate.toISOString(), res)
-    });
+  programmeProvider.getProgramme(function (programme) {
+    programme.setSetbackOverride(untilDate);
+    writeProgramme(programme, res)
+  });
 }
 
 function addDayToDate(date) {
-    date.setDate(date.getDate() + 1);
-    return date;
+  date.setDate(date.getDate() + 1);
+  return date;
 }
 
 function setDateTimeToLocalMidnight(date) {
-    date.setHours(0);
-    date.setMinutes(0);
-    date.setSeconds(0);
-    date.setUTCMilliseconds(0);
-    return date;
+  date.setHours(0);
+  date.setMinutes(0);
+  date.setSeconds(0);
+  date.setUTCMilliseconds(0);
+  return date;
 }
 
 function setModeUntilDate(req, res, modeFunc) {
-    var untilParam = req.params.until;
+  var untilParam = req.params.until;
 
-    var dateExpr = /^(\d{4})-?(\d{2})-?(\d{2})T(\d{2}):?(\d{2}):?(\d{2})Z$/;
+  var dateExpr = /^(\d{4})-?(\d{2})-?(\d{2})T(\d{2}):?(\d{2}):?(\d{2})Z$/;
 
-    if(/^\d{13,14}$/.test(untilParam)) { // time in milliseconds UTC
-        var untilInMs = parseInt(req.params.until);
-        modeFunc(new Date(untilInMs), res);
-    }
-    else if(dateExpr.test(untilParam)) // fully qualified ISO8601 UTC
-    {
-        var matchedDate = untilParam.match(dateExpr);
-        var untilDate = new Date(Date.UTC(matchedDate[1], matchedDate[2], matchedDate[3], matchedDate[4], matchedDate[5], matchedDate[6]));
-        modeFunc(untilDate, res);
-    }
-    else {
-        var untilDate = addDayToDate(new Date());
-        untilDate = setDateTimeToLocalMidnight(untilDate);
-        modeFunc(untilDate, res);
-    }
+  if (/^\d{13,14}$/.test(untilParam)) { // time in milliseconds UTC
+    var untilInMs = parseInt(req.params.until);
+    modeFunc(new Date(untilInMs), res);
+  } else if (dateExpr.test(untilParam)) // fully qualified ISO8601 UTC
+  {
+    var matchedDate = untilParam.match(dateExpr);
+    var untilDate = new Date(Date.UTC(matchedDate[1], matchedDate[2], matchedDate[3], matchedDate[4], matchedDate[5], matchedDate[6]));
+    modeFunc(untilDate, res);
+  } else {
+    var untilDate = addDayToDate(new Date());
+    untilDate = setDateTimeToLocalMidnight(untilDate);
+    modeFunc(untilDate, res);
+  }
 }
 
 module.exports = {
 
-    setHeatingModeAuto : function(req, res) {
-        programmeProvider.getProgramme(function(programme) {
-            programme.setHeatingOn();
-            programme.clearOverride();
-            writeProgramme(programme, "OK. Mode set to AUTO.", res);
-        })
-    },
+  setHeatingModeAuto: function (req, res) {
+    programmeProvider.getProgramme(function (programme) {
+      programme.setHeatingOn();
+      programme.clearOverride();
+      writeProgramme(programme, res);
+    })
+  },
 
-    setHeatingModeOff : function(req, res) {
-        programmeProvider.getProgramme(function(programme) {
-            programme.setHeatingOff();
-            writeProgramme(programme, "OK. Mode set to OFF.", res);
-        });
-    },
+  setHeatingModeOff: function (req, res) {
+    programmeProvider.getProgramme(function (programme) {
+      programme.setHeatingOff();
+      writeProgramme(programme, res);
+    });
+  },
 
-    setComfortMode : function(req, res) {
-        setModeUntilDate(req, res, comfortUntilDate);
-    },
+  setComfortMode: function (req, res) {
+    setModeUntilDate(req, res, comfortUntilDate);
+  },
 
-    setSetbackMode : function(req, res) {
-        setModeUntilDate(req, res, setbackUntilDate);
-    }
+  setSetbackMode: function (req, res) {
+    setModeUntilDate(req, res, setbackUntilDate);
+  }
 }
--- webapp/routes/programmeMode_api.js
+++ webapp/routes/programmeMode_api.js
@@ -3,11 +3,17 @@
 const router = require('express').Router();
 const programmeapi = require('../api/programme');
 const requiresApiAuthorization = require('../auth/requiresApiAuthorization');
+const auth = require('../auth/Authentication');
 
 // TODO: should be POST/PUT really - make it RESTful!
 router.get('/api/programme/setMode/auto', requiresApiAuthorization(), programmeapi.setHeatingModeAuto);
 router.get('/api/programme/setMode/heatingOff', requiresApiAuthorization(), programmeapi.setHeatingModeOff);
 router.get('/api/programme/setMode/comfort/:until?', requiresApiAuthorization(), programmeapi.setComfortMode);
 router.get('/api/programme/setMode/setback/:until?', requiresApiAuthorization(), programmeapi.setSetbackMode);
+
+router.post('/api/programme/setMode/auto', auth.getBearerHandler(), programmeapi.setHeatingModeAuto);
+router.post('/api/programme/setMode/heatingOff', auth.getBearerHandler(), programmeapi.setHeatingModeAuto);
+router.post('/api/programme/setMode/comfort', auth.getBearerHandler(), programmeapi.setComfortMode);
+router.post('/api/programme/setMode/setback', auth.getBearerHandler(), programmeapi.setSetbackMode);
 
 module.exports = router;
