1b5508f340266e06f310e4b7006088ec724173ae
Ross Harper <rossharper@users.noreply.github.com>
2017-01-03 13:11:01 +0000

    Add skeleton passport strategy for access tokens

--- webapp/auth/Authentication.js
+++ webapp/auth/Authentication.js
@@ -4,34 +4,38 @@ const passport = require('passport');
 const userRepository = require('./userRepository');
 const localStrategy = require('./LocalStrategy');
 const bearerStrategy = require('./BearerStrategy');
+const accessTokenStrategy = require('./accessTokenStrategy');
 
 function initPassport() {
-    passport.serializeUser((user, done) => {
-        done(null, user.username);
-    });
+  passport.serializeUser((user, done) => {
+    done(null, user.username);
+  });
 
-    passport.deserializeUser((id, done) => {
-        userRepository.findUser(id, (err, user) => {
-            done(err, user);
-        });
+  passport.deserializeUser((id, done) => {
+    userRepository.findUser(id, (err, user) => {
+      done(err, user);
     });
+  });
 
-    passport.use('local', localStrategy);
-    passport.use(bearerStrategy);
+  passport.use('local', localStrategy);
+  passport.use(bearerStrategy);
+  passport.use('accessToken', accessTokenStrategy);
 }
 
 const auth = {
-    initialize: function (app) {
-        initPassport();
-        app.use(passport.initialize());
-        app.use(passport.session());
-    },
-    getAuthenticationHandler: function (authenticationRedirects) {
-        return passport.authenticate('local', authenticationRedirects);
-    },
-    getBearerHandler: function() {
-      return passport.authenticate('bearer', { session: false });
-    }
+  initialize: function (app) {
+    initPassport();
+    app.use(passport.initialize());
+    app.use(passport.session());
+  },
+  getAuthenticationHandler: function (authenticationRedirects) {
+    return passport.authenticate('local', authenticationRedirects);
+  },
+  getBearerHandler: function () {
+    return passport.authenticate(['bearer', 'accessToken'], {
+      session: false
+    });
+  }
 };
 
 module.exports = auth;
--- webapp/auth/BearerStrategy.js
+++ webapp/auth/BearerStrategy.js
@@ -4,17 +4,23 @@ const BearerStrategy = require('passport-http-bearer');
 const tokenRepository = require('./tokenRepository');
 
 module.exports = new BearerStrategy(
-  function(token, done) {
-    tokenRepository.findToken(token, function (err, token) {
-      if (err) { return done(err); }
-      if (!token) { return done(null, false); }
+  (tokenValue, done) => {
+    tokenRepository.findToken(tokenValue, (err, token) => {
+      if (err) {
+        return done(err);
+      }
+      if (!token) {
+        return done(null, false);
+      }
       if (token.expiry <= new Date()) {
         tokenRepository.deleteToken(token, () => {
-            return done(null, false);
+          return done(null, false);
         });
         return;
       }
-      return done(null, token, { scope: 'all' });
+      return done(null, token, {
+        scope: 'all'
+      });
     });
   }
 );
