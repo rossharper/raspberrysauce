cacf7c162ae6340c5745117b44913db3fa46d23e
Ross Harper <rossharper@users.noreply.github.com>
2018-12-20 16:52:10 +0000

    Only redirect on fail

--- webapp/auth/requiresAuthorizedUser.js
+++ webapp/auth/requiresAuthorizedUser.js
@@ -4,7 +4,8 @@ module.exports = function () {
   return function (req, res, next) {
     if (req.isAuthenticated()) {
         return next();
+    } else {
+      res.redirect('/login');
     }
-    res.redirect('/login');
   };
 };
