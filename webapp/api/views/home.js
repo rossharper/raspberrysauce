8954c57d80ce2c69724b6f195cd3deddea835c96
Ross Harper <ross.harper@bbc.co.uk>
2018-12-23 19:41:16 +0000

    Return calling for heat status in iOS view

--- webapp/api/views/home.js
+++ webapp/api/views/home.js
@@ -1,25 +1,48 @@
+'use strict';
+
+const async = require('async');
 const batteryFile = require('../../models/batteryFile');
 const temperatureFile = require('../../models/temperatureFile');
+const callingForHeatFile = require('../../models/callingForHeatFile');
 const programmeProvider = require('../../models/programmeProvider');
 const programmeModelBuilder = require('../../models/programmeModelBuilder');
 
 module.exports = {
   getView: function (req, res, next) {
-    batteryFile.readFromFile((voltage) => {
-      temperatureFile.readFromFile((err, temperature) => {
-        if (err) {
-          next(err);
-          return;
-        }
-        programmeProvider.getProgramme((programme) => {
-          const view = {
-            temperature: temperature.temperature,
-            batteryVoltage: voltage.batteryVoltage,
-            programme: programmeModelBuilder.buildFromProgramme(programme)
-          };
-          res.send(view);
+
+    async.parallel({
+      callingForHeat: function (callback) {
+        callingForHeatFile.readFromFile((err, callingForHeat) => {
+          callback(err, callingForHeat);
         });
-      });
+      },
+      batteryVoltage: function (callback) {
+        batteryFile.readFromFile((voltage) => {
+          callback(null, voltage);
+        });
+      },
+      temperature: function (callback) {
+        temperatureFile.readFromFile((err, temperature) => {
+          callback(err, temperature);
+        });
+      },
+      programme: function (callback) {
+        programmeProvider.getProgramme((programme) => {
+          callback(null, programme);
+        });
+      }
+    }, (err, results) => {
+      if (err) {
+        next(err);
+        return;
+      }
+      const view = {
+        temperature: results.temperature.temperature,
+        batteryVoltage: results.batteryVoltage.batteryVoltage,
+        programme: programmeModelBuilder.buildFromProgramme(results.programme),
+        callingForHeat: results.callingForHeat
+      };
+      res.send(view);
     });
   }
 };
