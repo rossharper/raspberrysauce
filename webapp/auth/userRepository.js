c4d03ad091e5b40b53313285c8cb7451d8ae9b89
Ross Harper <rossharper@users.noreply.github.com>
2020-05-03 18:16:52 +0100

    Ensure users directory exists

--- webapp/auth/userRepository.js
+++ webapp/auth/userRepository.js
@@ -1,11 +1,30 @@
 'use strict';
 
 const jsonfile = require('jsonfile');
+const fs = require('fs');
 
 const PATH = '/var/lib/homecontrol/webapp/users/';
 
+function ensureDataDirectoryExists(path, callback) {
+  fs.mkdir(path, { recursive: true }, (err) => {
+    if (err) {
+      if (err.code === 'EEXIST') callback(null); // ignore the error if the folder already exists
+      else callback(err); // something else went wrong
+    } else callback(null); // successfully created folder
+  });
+}
+
 module.exports = {
   addUser: function (user, cb) {
+    ensureDataDirectoryExists(PATH, (err) => {
+      if (err) {
+        cb(err);
+      } else {
+        jsonfile.writeFile(PATH + user.username, user, (err) => {
+          cb(err);
+        });
+      }
+    });
     jsonfile.writeFile(PATH + user.username, user, (err) => {
       cb(err);
     });
