3fae7b5b1790115b8153017c73b8a768c493fd06
Ross Harper <rossharper@users.noreply.github.com>
2020-05-03 15:22:48 +0100

    Change install to pm2

--- installDaemon.sh
+++ installDaemon.sh
@@ -1,1 +1,4 @@
-forever-service install raspberrysauce -s index.js -e NODE_ENV=production --start -r pi
+#forever-service install raspberrysauce -s index.js -e NODE_ENV=production --start -r pi
+
+pm2 start index.js --env production --name raspberrysauce -l ~/homecontrol/logs/raspberrysauce
+pm2 save
