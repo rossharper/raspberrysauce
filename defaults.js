f00ab3f3e9a7b0eb9e433dc73deef185dc0b63e3
Ross Harper <ross@rosss-mbp-16.home>
2020-11-25 23:46:06 +0000

    Correct default sensor data path

--- defaults.js
+++ defaults.js
@@ -2,7 +2,7 @@
 
 module.exports = {
         SERVE_INSECURE: false,
-        SENSOR_DATA_PATH: '/var/lib/homecontrol/sensordata/temperatureSensors/TA',
+        SENSOR_DATA_PATH: '/var/lib/homecontrol/sensordata',
         PROGRAMME_DATA_PATH: '/var/lib/homecontrol/programdata',
         WEBAPP_DATA_PATH: '/var/lib/homecontrol/webapp'
 };
