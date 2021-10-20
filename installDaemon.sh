f19707de1dfbf04d73fe8729fd2691cd9bf00afe
Ross Harper <ross@rosss-mbp-16.home>
2021-10-20 20:33:53 +0100

    Update install daemon to serve insecure (proxy SSL termination)

--- installDaemon.sh
+++ installDaemon.sh
@@ -1,4 +1,6 @@
 #forever-service install raspberrysauce -s index.js -e NODE_ENV=production --start -r pi
 
-pm2 start index.js --env production --name raspberrysauce -l ~/homecontrol/logs/raspberrysauce
+#pm2 start index.js --env production --name raspberrysauce -l ~/homecontrol/logs/raspberrysauce
+# setup to serve insecure as SSL is currently terminated at reverse proxy
+pm2 start index.js --env production --name raspberrysauce -l ~/homecontrol/logs/raspberrysauce -- -i
 pm2 save
